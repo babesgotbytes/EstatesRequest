13983971320682e38471d5df637a4635a06a9b12
josephine <josephinemachage71@gmail.com>
2019-02-18 22:18:28 -0800

    gogog

---
+++ Departments/build/generated/src/org/apache/jsp/McomplainPage_jsp.java
@@ -0,0 +1,111 @@
+package org.apache.jsp;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.jsp.*;
+
+public final class McomplainPage_jsp extends org.apache.jasper.runtime.HttpJspBase
+    implements org.apache.jasper.runtime.JspSourceDependent {
+
+  private static final JspFactory _jspxFactory = JspFactory.getDefaultFactory();
+
+  private static java.util.List<String> _jspx_dependants;
+
+  private org.glassfish.jsp.api.ResourceInjector _jspx_resourceInjector;
+
+  public java.util.List<String> getDependants() {
+    return _jspx_dependants;
+  }
+
+  public void _jspService(HttpServletRequest request, HttpServletResponse response)
+        throws java.io.IOException, ServletException {
+
+    PageContext pageContext = null;
+    HttpSession session = null;
+    ServletContext application = null;
+    ServletConfig config = null;
+    JspWriter out = null;
+    Object page = this;
+    JspWriter _jspx_out = null;
+    PageContext _jspx_page_context = null;
+
+    try {
+      response.setContentType("text/html;charset=UTF-8");
+      pageContext = _jspxFactory.getPageContext(this, request, response,
+      			null, true, 8192, true);
+      _jspx_page_context = pageContext;
+      application = pageContext.getServletContext();
+      config = pageContext.getServletConfig();
+      session = pageContext.getSession();
+      out = pageContext.getOut();
+      _jspx_out = out;
+      _jspx_resourceInjector = (org.glassfish.jsp.api.ResourceInjector) application.getAttribute("com.sun.appserv.jsp.resource.injector");
+
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("\r\n");
+      out.write("<!DOCTYPE html>\r\n");
+      out.write("<html>\r\n");
+      out.write("    <head>\r\n");
+      out.write("        <meta http-equiv=\"Content-Type\" content=\"text/html; charset=UTF-8\">\r\n");
+      out.write("        <title>Complain</title>\r\n");
+      out.write("        <link rel=\"stylesheet\" href=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css\" integrity=\"sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO\" crossorigin=\"anonymous\">\r\n");
+      out.write("<script src=\"https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js\" integrity=\"sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy\" crossorigin=\"anonymous\"></script>\r\n");
+      out.write("    </head>\r\n");
+      out.write("    <body class=\"container-fluid\">\r\n");
+      out.write("        <nav class=\"navbar navbar-expand-lg navbar-light bg-light\">\r\n");
+      out.write("  <button class=\"navbar-toggler\" type=\"button\" data-toggle=\"collapse\" data-target=\"#navbarTogglerDemo03\" aria-controls=\"navbarTogglerDemo03\" aria-expanded=\"false\" aria-label=\"Toggle navigation\">\r\n");
+      out.write("    <span class=\"navbar-toggler-icon\"></span>\r\n");
+      out.write("  </button>\r\n");
+      out.write("  <a class=\"navbar-brand\" href=\"#\" style=\"font-size: 26px;\"></a>\r\n");
+      out.write("\r\n");
+      out.write("  <div class=\"collapse navbar-collapse\" id=\"navbarTogglerDemo03\">\r\n");
+      out.write("    <ul class=\"navbar-nav mr-auto mt-2 mt-lg-0\">\r\n");
+      out.write("      <li class=\"nav-item active\">\r\n");
+      out.write("        <a class=\"nav-link\" href=\"#\" style=\"font-size: 22px;\">Home <span class=\"sr-only\">(current)</span></a>\r\n");
+      out.write("      </li>\r\n");
+      out.write("      <li class=\"nav-item\">\r\n");
+      out.write("        <a class=\"nav-link\" href=\"index.html\" style=\"font-size: 20px;\">MakeReport</a>\r\n");
+      out.write("      </li>\r\n");
+      out.write("      <li class=\"nav-item\">\r\n");
+      out.write("        <a class=\"nav-link\" href=\"feedBackPage.jsp\" style=\"font-size: 20px;\">FeedBack</a>\r\n");
+      out.write("      </li>\r\n");
+      out.write("      <li class=\"nav-item\">\r\n");
+      out.write("        <a class=\"nav-link\" href=\"McomplainPage.jsp\" style=\"font-size: 20px;\">Make Complain</a>\r\n");
+      out.write("      </li>\r\n");
+      out.write("      <li class=\"nav-item\">\r\n");
+      out.write("        <a class=\"nav-link\" href=\"USerResetPwdPage.jsp\" style=\"font-size: 20px;\">Resetpassword</a>\r\n");
+      out.write("      </li>\r\n");
+      out.write("      <li class=\"nav-item\">\r\n");
+      out.write("        <a class=\"nav-link disabled\" href=\"UserLogout.jsp\" style=\"font-size: 19px\">Logout</a>\r\n");
+      out.write("      </li>\r\n");
+      out.write("    </ul>\r\n");
+      out.write("    \r\n");
+      out.write("  </div>\r\n");
+      out.write("</nav>\r\n");
+      out.write("</body>\r\n");
+      out.write("</html>\r\n");
+      out.write("\r\n");
+      out.write("    \r\n");
+      out.write("        <form method=\"post\" action=\"Mcomplain\" style=\"margin-left: 150px;margin-top: 60px\">\r\n");
+      out.write("            <h5>Send a Complain:</h5><br><br>\r\n");
+      out.write("        <textarea  class=\"form-control\" name=\"complain\" style=\"width: 300px;height: 160px\"></textarea>\r\n");
+      out.write("       \r\n");
+      out.write("        <button type=\"submit\"class=\"btn btn-primary\" id=\"butty\" style=\"margin-left: 120px;margin-top: 45px;font-size: 18px;font-family: serif;\"><i>Send</i></button>\r\n");
+      out.write("        </form>\r\n");
+      out.write("        </body>\r\n");
+      out.write("</html>\r\n");
+      out.write("\r\n");
+    } catch (Throwable t) {
+      if (!(t instanceof SkipPageException)){
+        out = _jspx_out;
+        if (out != null && out.getBufferSize() != 0)
+          out.clearBuffer();
+        if (_jspx_page_context != null) _jspx_page_context.handlePageException(t);
+        else throw new ServletException(t);
+      }
+    } finally {
+      _jspxFactory.releasePageContext(_jspx_page_context);
+    }
+  }
+}
